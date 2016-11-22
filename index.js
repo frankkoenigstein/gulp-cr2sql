d2a36e73b3d80658707d612cba5661077de275af
Frank Koenigstein <frank.koenigstein@dff-solutions.de>
2016-11-22 12:09:44 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict';
+const through = require('through2');
+const gutil = require('gulp-util');
+const path = require('path');
+const Vinyl = require('vinyl');
+const dateFormat = require('dateformat');
+
+var filenamePattern = new RegExp(/^([a-z]+)_([0-9]{6})_(.*)\.txt$/);
+var joinedBuffer = new Buffer('');
+
+function compile(options) {
+    options = options || {};
+
+function quote(val) {
+    if (typeof val === 'string') {
+        return "'" + val + "'";
+    }
+    else if (val === undefined) {
+        return null;
+    }
+
+    return val;
+}
+
+    function bufferContents(file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+        else if (file.isStream()) {
+            cb(new gutil.PluginError('Streaming not supported'));
+            return;
+        }
+
+        try {
+            var tMessage = null;
+            var tClassName = null;
+            var crMid = null;
+            var content = JSON.parse(file.contents.toString());
+            var crdate = content.date;
+            var filename = path.basename(file.path);
+            var fnmatches = filenamePattern.exec(filename);
+            crMid = fnmatches[2]
+            var t = content.throwable;
+
+            if (t) {
+                tMessage = t.message;
+                tClassName = t.className;
+            }
+
+            var insertStmt =
+                'insert into '
+                + (options.table || 'crashreport')
+                + ' (mid, ts, ex, msg) values ('
+                + quote(crMid) + ', '
+                + quote(crdate) + ', '
+                + quote(tClassName) + ', '
+                + quote(tMessage)
+                + ');'
+
+            joinedBuffer = Buffer.concat([joinedBuffer, new Buffer(insertStmt + '\n')]);
+
+            //console.log(insertStmt);
+        }
+        catch (err) {
+            this.emit('error', new gutil.PluginError('crashreports', err, { fileName: file.path }));
+        }
+
+        cb();
+    }
+
+    function endStream(cb) {
+        var path = (options.filename || 'crashreport_' + dateFormat(new Date(), 'yyyy-mm-dd_HH-MM-ss') + ".sql");
+
+        var joinedFile = new Vinyl({
+            cwd: __dirname,
+            path: path,
+            contents: new Buffer(joinedBuffer)
+        });
+
+        this.push(joinedFile);
+        cb();
+    }
+
+    return through.obj(bufferContents, endStream);
+}
+
+module.exports = function (options) {
+    return compile(options);
+};
